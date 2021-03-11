baff128941a8efc79c86b535830bdbfc062eeb97
Luiz Gadelha <luiz.gadelha@uni-jena.de>
2021-03-11 09:43:08 +0100

    Added Parsl config examples for using SDumont

---
+++ configs/sdumont_config_examples/sdumont_mesca2.py
@@ -0,0 +1,41 @@
+from parsl.config import Config
+from parsl.channels import LocalChannel
+from parsl.providers import SlurmProvider
+from parsl.executors import HighThroughputExecutor
+from parsl.launchers import SrunLauncher
+from parsl.addresses import address_by_hostname
+# from parsl.addresses import address_by_interface
+
+""" This config uses the HighThroughputExecutor and the SlurmProvider to
+run pilot jobs on the nodes. These are controlled by the Parsl from the login
+node using SLURM and communication between the workers on nodes and Parsl on
+the login node.
+"""
+config = Config(
+    executors=[
+        HighThroughputExecutor(
+            label="sdumont_htex_mesca2_1w",
+            address=address_by_hostname(),
+            #address=address_by_interface('ib0'),
+            max_workers=10,          # Set number of workers per node
+            provider=SlurmProvider(
+                cmd_timeout=120,     # Add extra time for slow scheduler responses
+                nodes_per_block=1,
+                walltime='48:00:00',
+                partition='mesca2',
+                # script to the scheduler eg: '#SBATCH --constraint=knl,quad,cache'
+                scheduler_options='#SBATCH --cpus-per-task=240',
+
+                init_blocks=1,
+                min_blocks=1,
+                max_blocks=1,
+
+                # Command to be run before starting a worker, such as:
+                # 'module load Anaconda; source activate parsl_env'.
+                worker_init='module load samtools/1.9; module load bowtie2/2.3; module load bedtools/2.29.0; module load R/3.5.2_openmpi_2.0_gnu; module load perl/5.20',
+                launcher=SrunLauncher(),
+            ),
+        )
+    ],
+    strategy=None,
+)
