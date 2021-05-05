9eb63cd11a45c7f27c589c698aa8a120122107b4
Lucas Cruz <62730384+lucruzz@users.noreply.github.com>
2021-05-05 18:04:32 -0300

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -4,13 +4,15 @@
 #SBATCH --ntasks=1              #Numero de tarefas
 #SBATCH --cpus-per-task=24      #Numero de threads por tarefa
 #SBATCH -p nvidia_long          #Fila (partition) a ser utilizada
-#SBATCH --time=05:30:00         #Altera o tempo limite do job
+#SBATCH --time=01:15:00         #Altera o tempo limite do job
 #SBATCH -J RNA-Seq              #Nome job
 #SBATCH --exclusive             #Uso do nó de forma exclusiva
 
 #Carregar os módulos
 module load bowtie2/2.3
-module load python/3.7.2
+module load samtools/1.10_gnu
+module load python/3.8.2
+module load java/jdk-8u201
 module load R/3.5.2_openmpi_2.0_gnu
 
 #Acessa o diretório onde o script está localizado
@@ -20,12 +22,14 @@ cd $DIR
 #Permissão para acessar o script DESeq.R
 chmod a+x $DIR/DESeq.R
 
-SCRIPT_PYTHON=$DIR/rna-seq.py
+SCRIPT_PYTHON=$DIR/RNA-seq.py
 BASE_GENOMICA=$DIR/mm9/mm9
-MULTITHREAD_BOWTIE=6
-INPUTS=$DIR/inputs/SRR
-OUTPUTS=$DIR/outputs/
+MULTITHREAD=12
+INPUTS=$DIR/inputs
+OUTPUTS=$DIR/outputs
 GTF=$DIR/Mus_musculus.NCBIM37.67.gtf
 DESEQ=$DIR/DESeq.R
+PICARD_JAVA_FILE=$DIR/picard/build/libs/picard.jar
+MERGE_HTSEQ=$DIR/HTSeq_Merge.py
 
-time python3 $SCRIPT_PYTHON $BASE_GENOMICA $MULTITHREAD_BOWTIE $INPUTS $OUTPUTS $GTF $DESEQ
+time python3 $SCRIPT_PYTHON $BASE_GENOMICA $MULTITHREAD_BOWTIE $INPUTS $OUTPUTS $GTF $DESEQ $PICARD_JAVA_FILE $MERGE_HTSEQ
