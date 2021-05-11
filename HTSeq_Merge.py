f8c86ee3ad60abbbef3ecf93ae233b8f71c4bbae
Lucas Cruz <62730384+lucruzz@users.noreply.github.com>
2021-05-11 13:41:43 -0300

    included n_colummns argument

--- HTSeq_Merge.py
+++ HTSeq_Merge.py
@@ -1,11 +1,11 @@
 # Script pega a saída partida do HTSeq com 25 partes e une em 2 colunas
 # Basicamente, ele vai somar as leituras já mapeadas pelo HTSeq
-# Para executar: python3 HTSeq_Merge.py <<filename_input>>.count <<filename_output>>.merge.count <<n_splited_files>>
+# Para executar: python3 HTSeq_count.py <<filename_input>>.count <<filename_output>>.merge.count <<n_splited_files>>
 import sys
 
 file_name_in = sys.argv[1]
 file_name_out = sys.argv[2]
-n_colummns = sys.argv[3]
+n_colummns = int(sys.argv[3])
 
 HTSeq_file = open(file_name_in, "r")
 HTSeq_new_file = open(file_name_out, "w")
