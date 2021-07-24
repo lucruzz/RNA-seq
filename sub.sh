5dc63f6ddf46b60d09e3d8320b791b58edc6ba86
Lucas Cruz <62730384+lucruzz@users.noreply.github.com>
2021-07-23 21:27:08 -0300

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#SBATCH --nodes=1               #Numero de Nós
+#SBATCH --nodes=6               #Numero de Nós
 #SBATCH --ntasks-per-node=1     #Numero de tarefas por Nó
-#SBATCH --ntasks=1              #Numero de tarefas
+#SBATCH --ntasks=6              #Numero de tarefas
 #SBATCH --cpus-per-task=24      #Numero de threads por tarefa
 #SBATCH -p nvidia_long          #Fila (partition) a ser utilizada
-#SBATCH --time=01:15:00         #Altera o tempo limite do job
+#SBATCH --time=00:30:00         #Altera o tempo limite do job
 #SBATCH -J RNA-Seq              #Nome job
 #SBATCH --exclusive             #Uso do nó de forma exclusiva
 
 #Carregar os módulos
+module load picard/2.18
 module load bowtie2/2.3
 module load samtools/1.10_gnu
 module load python/3.8.2
@@ -29,7 +30,5 @@ INPUTS=$DIR/inputs
 OUTPUTS=$DIR/outputs
 GTF=$DIR/Mus_musculus.NCBIM37.67.gtf
 DESEQ=$DIR/DESeq.R
-PICARD_JAVA_FILE=$DIR/picard/build/libs/picard.jar
-MERGE_HTSEQ=$DIR/HTSeq_Merge.py
 
-time python3 $SCRIPT_PYTHON $BASE_GENOMICA $MULTITHREAD $INPUTS $OUTPUTS $GTF $DESEQ $PICARD_JAVA_FILE $MERGE_HTSEQ
+time python3 $SCRIPT_PYTHON $BASE_GENOMICA $MULTITHREAD $INPUTS $OUTPUTS $GTF $DESEQ
