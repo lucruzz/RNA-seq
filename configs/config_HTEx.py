2e78aadaeca7f3145ed01098a5b3d0ba1a49ec8e
Lucas Cruz <62730384+lucruzz@users.noreply.github.com>
2022-05-10 07:17:28 -0300

    Update config_HTEx.py

--- configs/config_HTEx.py
+++ configs/config_HTEx.py
@@ -1,35 +1,46 @@
 from parsl.config import Config
-#from parsl.monitoring.monitoring import MonitoringHub
 from parsl.providers import SlurmProvider
 from parsl.executors import HighThroughputExecutor
 from parsl.launchers import SrunLauncher
-from parsl.addresses import address_by_interface#address_by_hostname
-#from parsl.executors.threads import ThreadPoolExecutor
-#from parsl.data_provider.file_noop import NoOpFileStaging
-#from parsl.data_provider.data_manager import default_staging
+from parsl.addresses import address_by_interface
+from parsl.data_provider.data_manager import default_staging
+
+partition_queue = 'cpu_dev'
+total_time = '00:20:00'
+nodes = 1
+workers=24
+over_rides='-c 24'
+python='module load python/3.8.2\n'
+bowtie='module load bowtie2/2.3\n'
+sort='module load samtools/1.10_gnu\n'
+picard='module load picard/2.18\n'
+r='module load R/3.5.2_openmpi_2.0_gnu\n'
+modules = python + bowtie + sort + picard + r
+job_name='#SBATCH -J SSD\n'
 
 config = Config(
     executors=[
         HighThroughputExecutor(
-            address=address_by_interface('ib0'),#address_by_hostname(),
+            address=address_by_interface('ib0'), # address_by_hostname(),
             label='htex',
-            cores_per_worker=24, # number of cores used by one task
-            max_workers=24, # number of cores per node           
+            cores_per_worker=workers, # number of cores used by one task
+            max_workers=workers, # number of cores per node
             provider=SlurmProvider(
-                partition='nvidia_long',
-                nodes_per_block=6, # number of nodes
-                cmd_timeout = 120, # duration for which the provider will wait for a command to be invoked on a remote system
+                partition=partition_queue,
+                nodes_per_block=nodes, # number of nodes
+                cmd_timeout = 240,
                 init_blocks=1,
-                launcher=SrunLauncher(overrides='-c 24'),
+                launcher=SrunLauncher(overrides=over_rides),
                 max_blocks=1,
                 min_blocks=1,
                 parallelism=1,
                 move_files=False,
-                scheduler_options = '#SBATCH -J WfRNAseq\n',
-                walltime='01:00:00',
-                worker_init='module load picard/2.18\nmodule load bowtie2/2.3\nmodule load samtools/1.10_gnu\nmodule load python/3.8.2\nmodule load java/jdk-8u201\nmodule load R/3.5.2_openmpi_2.0_gnu\n'
+                scheduler_options = job_name,
+                walltime=total_time,
+                worker_init=modules
             ),
-            #storage_access=default_staging,
+            storage_access=default_staging,
+            working_dir='tmp/'
         ),
     ],
     strategy='simple'
